7759a58041af8a7156d14992a1ec47ee08f57c98
Aakash R. Anand <aanand@smartcarenet.com>
2018-04-16 23:08:44 +0530

    New Change

--- Add.java
+++ Add.java
@@ -3,6 +3,8 @@ public static void main(String[] args){
 int a=50;
 int b=60;
 int c=a+b;{
-System.out.print("Addition of a and b is "+c);}
+System.out.print("Addition of a and b is "+c);
+System.out.print(a);
+}
 }
 }
